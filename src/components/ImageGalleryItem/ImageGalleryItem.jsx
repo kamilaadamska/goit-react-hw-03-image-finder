5db6585a82c041d569951071830ae716c3c093d1
Kamila Adamska <121kama121@gmail.com>
2023-07-01 00:54:02 +0200

    Added ImageGalleryItem component

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,7 @@
+import css from './imagegalleryitem.module.css';
+
+export const ImageGalleryItem = () => {
+  <li className={css.photoCard}>
+    <img src="" alt="" className={css.photo} />
+  </li>;
+};
